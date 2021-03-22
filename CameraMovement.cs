cc2cae7a68ab41756f79092a0b20e98dbd56cb02
Caroline <81135719+carolinegraham6622@users.noreply.github.com>
2021-03-22 00:13:39 -0400

    Adding 3 .cs files for basic RPG game

---
+++ CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPos;
+    public Vector2 minPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // update always comes last, camera goes in late update (this prevents jerky camera)
+    void FixedUpdate()
+    {
+        if (transform.position != target.position)
+        {
+
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
+
+            //find distance between it and target and move percentage of distance each time
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+ 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Vector3.zero; //reset 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.Equals(Vector3.zero) == false) //moving character
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+
+        
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
